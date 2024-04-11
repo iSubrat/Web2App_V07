873d57d2ed20c7791564c4c7868651d5e26109db
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-04-11 20:49:59 +0530

    Update step02.py

--- step02.py
+++ step02.py
@@ -52,7 +52,7 @@ def execute_query(db_host, db_username, db_password, db_database, query):
             content = f'''{
     "appconfiguration": '{
         "app_name": "{app_name}",
-        "url": "{web_url}",
+        "url": "{web_url.replace('/', '\/')}",
         "appLanuguage": "en",
         "isJavascriptEnable": "true",
         "isSplashScreen": "false",
