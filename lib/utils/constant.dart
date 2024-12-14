cbaad76fc03fddcaf6ff9b87d3161e08257471b2
GitHub Action <action@github.com>
2024-12-14 17:04:57 +0000

    Update Android Manifest

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,5 +1,5 @@
 //With PHP backend
-const BASE_URL = "https://web2app.appcollection.in/downloads/01_Profiles/10581";
+const BASE_URL = "https://web2app.appcollection.in/downloads/01_Profiles/10587";
 
 //without PHP backend
 const PURCHASE_CODE = '';
